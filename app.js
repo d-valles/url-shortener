0f297c0cfd2f22874d6c447c41fbacfaa24987a7
David Valles <david.valles@wizeline.com>
2018-03-16 10:55:27 -0600

    Finished tutorial

--- app.js
+++ app.js
@@ -1,22 +1,68 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config');
+var base58 = require('./base58.js');
 
-const path = require('path');
+var Url = require('./models/urls');
+
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', ( req, res ) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.post('/api/shorten', ( req, res ) => {
+app.post('/api/shorten', function(req, res){
+  var longUrl = req.body.url;
+  var shortUrl = '';
+
+  Url.findOne({long_url: longUrl}, function (err, doc){
+    if (doc){
+      shortUrl = config.webhost + base58.encode(doc._id);
+      res.send({'shortUrl': shortUrl});
+    } else {
+      var newUrl = Url({
+        long_url: longUrl
+      });
+
+      newUrl.save(function(err) {
+        if (err){
+          console.log(err);
+        }
+
+        shortUrl = config.webhost + base58.encode(newUrl._id);
+
+        res.send({'shortUrl': shortUrl});
+      });
+    }
+
+  });
 
 });
 
-app.get('/:encoded_id', ( req, res ) => {
+app.get('/:encoded_id', function(req, res){
+
+  var base58Id = req.params.encoded_id;
+
+  var id = base58.decode(base58Id);
+
+  Url.findOne({_id: id}, function (err, doc){
+    if (doc) {
+      res.redirect(doc.long_url);
+    } else {
+      res.redirect(config.webhost);
+    }
+  });
 
 });
 
-const server = app.listen(3000, () => {
-    console.log('Server listening port 3000');
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
 });
--- models/urls.js
+++ models/urls.js
@@ -1,32 +1,31 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-const CounterSchema = Schema({
-    _id: { type: String, require: true },
+var CounterSchema = Schema({
+    _id: {type: String, required: true},
     seq: { type: Number, default: 0 }
 });
 
-const counter = mongoose.model('counter', CounterSchema);
+var counter = mongoose.model('counter', CounterSchema);
 
-const urlSchema = new Schema({
-    _id: {  type: Number, index: true },
-    long_url: String,
-    created_at: Date
+// create a schema for our links
+var urlSchema = new Schema({
+  _id: {type: Number, index: true},
+  long_url: String,
+  created_at: Date
 });
 
-urlSchema.pre('save', (next) => {
-    const doc = this;
-    counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, (error, counter) => {
-        if (error)
-            return next(error);
-        doc.created_at = new Date();
-        doc._id = counter.seq;
-        next();
-    });
+urlSchema.pre('save', function(next){
+  var doc = this;
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+      if (error)
+          return next(error);
+      doc.created_at = new Date();
+      doc._id = counter.seq;
+      next();
+  });
 });
 
-const Url = mongoose.model('Url', urlSchema);
+var Url = mongoose.model('Url', urlSchema);
 
-export default {
-    Url
-}
+module.exports = Url;
